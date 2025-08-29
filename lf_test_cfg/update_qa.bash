123151330717e86bbe18072f8b433e19e4bc2726
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2025-08-29 11:07:29 -0600

    removed update_qa.bash
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

--- lf_test_cfg/update_qa.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# update the QA json files
-
-cp ~/git/LanforgeQA/lf_test_cfg/*.bash  ~/git/lanforge-scripts/py-scripts/tools
-cp -r ~/git/LanforgeQA/lf_test_cfg/ct_dut_json ~/git/lanforge-scripts/py-scripts/tools
-cp -r ~/git/LanforgeQA/lf_test_cfg/ct_rig_json ~/git/lanforge-scripts/py-scripts/tools
-cp -r ~/git/LanforgeQA/lf_test_cfg/ct_tests_json ~/git/lanforge-scripts/py-scripts/tools
-
