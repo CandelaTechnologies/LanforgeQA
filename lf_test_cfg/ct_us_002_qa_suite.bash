6cb69789fdcc3e3f714419bb6454be87742b71d0
Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>
2026-04-24 11:44:26 -0600

    lf_test_cfg/ct_us_002_qa_suite.bash
    initial commit
    
    Signed-off-by: Chuck SmileyRekiere <chuck.smileyrekiere@candelatech.com>

---
+++ lf_test_cfg/ct_us_002_qa_suite.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#!/bin/bash
+
+# allow commands to be printed to the terminal
+set -x
+
+echo "Running Wifi Capacity Tests"
+./ct_us_002_wc.bash
+
+echo "Running Dataplane Tests"
+./ct_us_002_dp.bash
+
+echo "Running ChamberView: Port Reset and Scale Tests"
+./ct_us_002_cv.bash
